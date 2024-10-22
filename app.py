be8198d44eed4623448b70dea0b362aa8c3279b2
JoeWangu <joewangu2021@outlook.com>
2024-10-22 13:05:34 +0300

    add cors

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, make_response, jsonify, request, url_for, redirect, ses
 # from models import User, Artwork, Review, app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from flask_cors import CORS
 import bcrypt
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
